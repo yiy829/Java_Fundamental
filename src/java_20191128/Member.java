e29be5407b0fd29d7662691f5410fb3bfa4cffa6
yiy82 <yiy82@DESKTOP-19JJ6NI>
2019-11-28 23:03:35 +0900

    2019-11-28-v1

--- src/java_20191128/Member.java
+++ src/java_20191128/Member.java
@@ -1,25 +1,28 @@
 package java_20191128;
 
 public class Member {
-	private String id;
-	private String name;
+	private static String id;
+	private static String name;
 	public Member(String id, String name) {
 		super();
 		this.id = id;
 		this.name = name;
 	}
-	public String getId() {
+	public static String getId() {
 		return id;
 	}
 	public void setId(String id) {
 		this.id = id;
 	}
-	public String getName() {
+	public static String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+	@Override
+	public String toString() {
+		return "아이디 =" + Member.getId() + ", 이름 =" + Member.getName()+"\t"+"\n";
+	}
 	
 }
--- src/java_20191128/MemberManagementDemo.java
+++ src/java_20191128/MemberManagementDemo.java
@@ -22,13 +22,13 @@ public class MemberManagementDemo {
 	public void run(String index){
 		if(index.equals("1")){
 			insert();
-		}else if(index.equals(2)){
+		}else if(index.equals("2")){
 			update();
-		}else if(index.equals(3)){
+		}else if(index.equals("3")){
 			delete();
-		}else if(index.equals(4)){
+		}else if(index.equals("4")){
 			search();
-		}else if(index.equals(5)){
+		}else if(index.equals("5")){
 			System.out.println("프로그램 종료");
 		}else{
 			System.out.println("사요나라!!");
@@ -40,9 +40,19 @@ public class MemberManagementDemo {
 	//4. 번호를 선택하세요> 메시지를 출력한 후 키보드 입력을 받게 대기 한다.
 	//5. 번호를 입력받으면 run()메서드를 호출한다.
 	public void search(){
-		
+		String id = console("검색할 아이디>");
+		for(Member p : list){
+			if(p.getId().equals(id)){
+				System.out.println(p);
+				continue;
+			}else{
+			System.out.println("검색된 결과가 없습니다.");
+			}
+		}
+		print();
+		String index = console("번호를 선택하세요>");
+		run(index);
 	}
-	
 	
 	//1. 삭제할 아이디를 입력받는다.
 	//2. ArrayList에 있는 멤버중에서 삭제할 아이디를 찾아서 있으면 삭제하고, 없으면 삭제할 아이디가 없습니다. 라는 메시지를 출력한다.
@@ -51,7 +61,21 @@ public class MemberManagementDemo {
 	//5. 번호를 선택하세요> 메시지를 출력한 후 키보드 입력을 받게 대기 한다.
 	//6. 번호를 입력받으면 run()메서드를 호출한다.
 	public void delete(){
+		String id = console("삭제할 아이디>");
 		
+		for(Member p : list){
+			if(p.getId().equals(id)){
+				list.remove(p);
+				System.out.println(p.toString());
+				System.out.println("성공적으로 삭제되었습니다.");
+				break;
+			}else{
+				System.out.println("삭제할 아이디가 없습니다.");
+			}
+		}
+		print();
+		String index = console("번호를 선택하세요>");
+		run(index);
 	}
 	
 	
@@ -63,8 +87,23 @@ public class MemberManagementDemo {
 	//5. 번호를 선택하세요> 메시지를 출력한 후 키보드 입력을 받게 대기 한다.
 	//6. 번호를 입력받으면 run()메서드를 호출한다.
 	public void update(){
-
+		String id = console("검색할 아이디>");
+		String modifyId = console("수정할 아이디>");
+		for(Member p : list){
+			if(p.getId().equals(id)){
+				p.setId(modifyId);
+				System.out.println(p.toString());
+				System.out.println("성공적으로 수정되었습니다.");
+				
+			}else{
+			System.out.println("수정할 아이디가 없습니다.");
+			}
+		}
+		print();
+		String index = console("번호를 선택하세요>");
+		run(index);
 	}
+	
 	
 	
 	//1. 아이디를 입력받는다.
@@ -81,7 +120,7 @@ public class MemberManagementDemo {
 		list.add(m);
 		
 		System.out.printf("총 회원은 %d명 입니다.%n", list.size());
-		
+		System.out.print(list.toString());
 		print();
 		String index = console("번호를 선택하세요>");
 		run(index);
