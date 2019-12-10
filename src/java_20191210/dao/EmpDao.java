6fd6e3928ce82d4f38bb0f974bfabe4a9a97cebc
yiy82 <yiy82@DESKTOP-19JJ6NI>
2019-12-11 01:35:03 +0900

    2019-12-11-v1

--- src/java_20191210/dao/EmpDao.java
+++ src/java_20191210/dao/EmpDao.java
@@ -1,5 +1,9 @@
 package java_20191210.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import java_20191210.dto.EmpDto;
@@ -13,39 +17,78 @@ public class EmpDao {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private static EmpDao single;
-	
+
 	private EmpDao() {
 	}
-	
+
 	public static EmpDao getInstance() {
-		if(single == null){
+		if (single == null) {
 			single = new EmpDao();
 		}
 		return single;
 	}
-	
+
 	public boolean insert(EmpDto eto) {
 		boolean isSuccess = false;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		try {
+			con = DriverManager.getConnection("jdbc:mysql://localhost/acorn", "acorn12", "acorn12");
+			StringBuffer sql = new StringBuffer();
+			sql.append();
+			sql.append();
+			sql.append();
+			pstmt = con.prepareStatement(sql.toString());
+
+			int index = 0;
+			pstmt.setInt(++index, eto.getNo());
+			pstmt.setString(++index, eto.getName());
+			pstmt.setString(++index, eto.getJob());
+			pstmt.setInt(++index, eto.getMgr());
+			pstmt.setInt(++index, eto.getHiredate());
+			pstmt.setInt(++index, eto.getSal());
+			pstmt.setInt(++index, eto.getComm());
+			pstmt.setInt(++index, eto.getDetpno());
+
+			pstmt.executeUpdate();
+			isSuccess = true;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null) pstmt.close();
+				if (con != null) con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 		return isSuccess;
 	}
-	
-	public boolean update(EmpDto eto){
+
+	public boolean update(EmpDto eto) {
 		boolean isSuccess = false;
+
 		return isSuccess;
 	}
-	
-	public boolean delete(int no){
+
+	public boolean delete(int no) {
 		boolean isSuccess = false;
+
 		return isSuccess;
 	}
+
 	public ArrayList<EmpDto> select() {
 		ArrayList<EmpDto> list = new ArrayList<EmpDto>();
+
 		return list;
 	}
-	public EmpDto select(int no){
-		EmpDto eto = null;
-		return eto;
+
+	public EmpDto select(int no) {
+		EmpDto dto = null;
+
+		return dto;
 	}
 }
